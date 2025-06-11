b1dc379f89b450ec30798f68630468660d1129c5
Pheonix55 <m33322ali@gmail.com>
2025-06-11 18:22:30 +0500

    .

--- nav_script.js
+++ nav_script.js
@@ -4,20 +4,24 @@ const sidebar = document.getElementById('sidebar');
 function toggleSubmenu(element) {
     const submenu = element.nextElementSibling;
     const isOpen = submenu.classList.contains('show');//if its open then it will be closed by closeAllSubmenu, if its not then the next condition wil  open it
-
+    // document.querySelectorAll('#sidebar > ul > li').forEach(li => li.classList.remove('active'));
     closeAllSubmenu();
 
     if (!isOpen) {
-        if (sidebar.getAttribute('data-style') == 'icon-only'){
+        if (sidebar.getAttribute('data-style') == 'icon-only') {
             sidebar.setAttribute('data-style', 'full');
         }
-            element.classList.add('rotate');
+        element.classList.add('active');
+        element.classList.add('rotate');
         submenu.classList.add('show');
     }
 }
 
 function closeAllSubmenu() {
     document.querySelectorAll('.dropdown-btn').forEach(el => el.classList.remove('rotate'));
+    document.querySelectorAll('#sidebar > ul > li').forEach(li => li.classList.remove('active'));
+    document.querySelectorAll('#sidebar > ul > li > button').forEach(li => li.classList.remove('active'));
+
     sidebar
         .querySelectorAll('.sub-menu')
         .forEach(el => el.classList.remove('show')
@@ -34,3 +38,11 @@ function toggleSidebar() {
 
     }
 }
+
+
+document.querySelectorAll('#sidebar > ul > li > a').forEach(link => {
+    link.addEventListener('click', function (e) {
+        document.querySelectorAll('#sidebar > ul > li').forEach(li => li.classList.remove('active'));
+        this.parentElement.classList.add('active');
+    });
+});
